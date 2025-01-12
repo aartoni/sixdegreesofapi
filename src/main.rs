d6f78d8dcbbb481c2e965293e48cc67a6ea9d40f
aartoni <alessio@artoni.org>
2025-01-12 17:59:43 +0700

    feat: add tracing and dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use axum::{Router, routing::get};
+use dotenvy::dotenv;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
+    dotenv()?;
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let app = Router::new().route("/", get(async || "Hello, World!"));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+    Ok(())
 }
