95ad3a876f5a8e302be734828b0ae257cb850848
aartoni <alessio@artoni.org>
2025-01-14 21:32:34 +0100

    feat: build response from query results

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use axum::{
     http::StatusCode,
 };
 pub use db::DatabaseBuilder;
-use neo4rs::query;
+use neo4rs::{Path, query};
 use serde::{Deserialize, Serialize};
 pub use state::AppState;
 
@@ -39,14 +39,40 @@ pub async fn paths(
     State(state): State<Arc<AppState>>,
     req: Query<PathRequest>,
 ) -> (StatusCode, Json<PathResponse>) {
-    let query = query("MATCH path=allShortestPaths((:Key {fingerprint: $from})-[*]-(:Key {fingerprint: $to})) RETURN path, length(path) as distance")
+    let query = query("MATCH path=allShortestPaths((:Key {fingerprint: $from})-[*]-(:Key {fingerprint: $to})) RETURN path")
         .param("from", req.from.as_str())
         .param("to", req.to.as_str());
 
-    let _results = state
+    let mut results = state
         .db
         .execute(query)
         .await
         .expect("Error while reading paths from DB.");
-    todo!()
+
+    let mut paths = Vec::new();
+
+    while let Ok(Some(row)) = results.next().await {
+        let path: Path = row.get("path").expect("Path not part of query results");
+        let nodes = path
+            .nodes()
+            .into_iter()
+            .flat_map(|n| n.get::<String>("fingerprint"))
+            .map(|fp| Node {
+                id: "".into(),
+                title: fp,
+                url: "".into(),
+                description: "".into(),
+                thumbnail: "".into(),
+            })
+            .collect();
+
+        paths.push(nodes);
+    }
+
+    let response = PathResponse {
+        paths,
+        source_friendly_name: todo!("Transform into spaced hex"),
+        target_friendly_name: todo!("Transform into spaced hex"),
+    };
+    (StatusCode::OK, response.into())
 }
